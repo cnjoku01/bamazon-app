f9b4e7320b2959da9a10ae5d46853071f771c098
Chi Njoku <chin.njoku@gmail.com>
2019-08-19 13:47:27 -0500

    adding manager view and sreenshots

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var table = require ("cli-table");
+var Table = require ("cli-table");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -36,4 +36,114 @@ var connection = mysql.createConnection({
       
   };
 
-  
+  function inquirerForUpdates(){
+	inquirer.prompt([{
+		name:"action",
+		type: "list",
+		message: "Choose an option below to manage current inventory:",
+		choices: ["Restock Inventory", "Add New Product", "Remove An Existing Product"]
+	}]).then(function(answers){
+		switch(answers.action){
+			case 'Restock Inventory':
+				restockRequest();
+				break;
+			case 'Add New Product':
+				addRequest();
+				break;
+			case 'Remove An Existing Product':
+				removeRequest();
+				break;		
+		}
+	});
+};
+
+function restockRequest(){
+	inquirer.prompt([
+	{
+		name:"ID",
+		type:"input",
+		message:"What is the item number of the item you would like to restock?"
+	},
+	{
+		name:"ItemQuantity",
+		type:"input",
+		message:"What is the quantity you would like to add?"
+	},
+	]).then(function(answers){
+		var quantityAdded = answers.quantity;
+		var IDOfProduct = answers.ID;
+		restockInventory(IDOfProduct, quantityAdded);
+	});
+};
+
+function restockInventory(id, quant){
+	connection.query('SELECT * FROM Products WHERE item_id = '+id, function(err,res){
+		if(err){console.log(err)};
+		connection.query('UPDATE Products SET stock_quantity = stock_quantity + ' +quantity+ 'WHERE item_id =' +item_id);
+
+		displayInventory();
+	});
+};
+
+function addRequest(){
+	inquirer.prompt([
+
+	{
+		name: "ID",
+		type: "input",
+		message: "Add ID Number"
+
+	},	
+	{
+		name: "Name",
+		type: "input",
+		message: "What is name of product you would like to stock?"
+	},
+	{
+		name:"Category",
+		type:"input",
+		message:"What is the department for product?"
+	},
+	{
+		name:"Price",
+		type:"input",
+		message:"What is the price for item?"
+	},
+	{
+		name:"Quantity",
+		type:"input",
+		message:"What is the quantity you would like to add?"
+	},
+
+	]).then(function(answers){
+		var id = answers.Id;
+		var name = answers.productName;
+		var category = answers.Category;
+		var price = answers.Price;
+		var quantity = answers.Quantity;
+		buildNewItem(id,name,category,price,quantity); 
+	});
+  };
+
+  function buildNewItem(name,category,price,quantity){
+  	connection.query('INSERT INTO products (item_id,productName,departmentName,price,stock_quantity) VALUES("' + id + '","' + name + '","' + department + '",' + price + ',' + quantity +  ')');
+  	displayInventory();
+  };
+
+  function removeRequest(){
+  	inquirer.prompt([{
+  		name:"ID",
+  		type:"input",
+  		message:"What is the item number of the item you would like to remove?"
+  	}]).then(function(answer){
+  		var id = answers.ID;
+  		removeInventory(id); 
+  	});
+  };
+
+  function removeInventory(id){
+  	connection.query('DELETE FROM Products WHERE item_id = ' + id);
+  	displayInventory();
+  };
+
+  displayInventory();
